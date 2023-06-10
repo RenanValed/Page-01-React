21fa3fa823ab23ca2b35024094cdec4949d96d12
RenanValed <RenanValed@gmail.com>
2023-06-10 11:41:02 -0300

    Evento de botao por props

--- app/src/components/Evento.js
+++ app/src/components/Evento.js
@@ -1,14 +1,22 @@
 import Style from './Evento.module.css'
+import Button from './evento/Button'
+
 
 function Evento(){
     function meuEvento(){
         console.log("Opa, fui ativado!")
     }
 
+    function meu2Evento(){
+        console.log("Opa, tambem fui!")
+    }
+
+
     return(
         <div className={Style.eventoContent}>
             <p> Clique aqui para disparar um evento:</p>
-            <button onClick={meuEvento}>Ativar!</button>
+            <Button event={meuEvento} text="Primeiro Evento"/>
+            <Button event={meu2Evento} text="Segundo Evento"/>
         </div>
     )
 }
