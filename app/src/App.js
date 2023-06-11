72e704576e427b70d76235342c1d0ffee2327de1
RenanValed <RenanValed@gmail.com>
2023-06-11 01:21:43 -0300

    final exercice adequation

--- app/src/App.js
+++ app/src/App.js
@@ -1,5 +1,5 @@
 import './App.css'
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Home from './pages/Home'
 import Contato from './pages/Contato'
 import Empresa from './pages/Empresa'
