578f02440b89365486b89dda0169c1c98cfa8ef5
RenanValed <RenanValed@gmail.com>
2023-06-11 11:43:56 -0300

    rodape icons

--- app/src/components/layout/Footer.js
+++ app/src/components/layout/Footer.js
@@ -1,7 +1,15 @@
 import styles from './Footer.module.css'
+import {FaFacebook, FaFacebookF, FaGithub, FaInstagram} from 'react-icons/fa'
 
 function Footer(){
-    return <footer className={styles.rodape}>Rodapé</footer>
+    return <footer className={styles.rodape}>
+        <ul className={styles.socialList}>
+            <li><FaFacebookF/></li>
+            <li><FaInstagram/></li>
+            <li><FaGithub/></li>
+        </ul>
+        <h3>Created by @Renanvaled</h3>
+        </footer>
 }
 
 export default Footer
